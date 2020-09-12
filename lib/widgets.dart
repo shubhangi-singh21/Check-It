76ad4815d6adab85af49ebf011424b1f7ee13ca6
shubhangi-singh21 <shubhangifas@gmail.com>
2020-09-12 15:21:54 +0530

    checkbox added

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskCardWidget extends StatelessWidget {
   final String title;
   final String desc;
+
   TaskCardWidget({this.title, this.desc});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -45,5 +48,44 @@ class TaskCardWidget extends StatelessWidget {
             )
           ],
         ));
+  }
+}
+
+class TodoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 6.0,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 20.0,
+            height: 20.0,
+            margin: EdgeInsets.only(
+              right: 12.0,
+            ),
+            decoration: BoxDecoration(
+              color: Color(0XFFFC9D9D),
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Image(
+              height: 20.0,
+              image: AssetImage('assets/images/tick.png'),
+            ),
+          ),
+          Text(
+            "Todo Widget",
+            style: TextStyle(
+              color: Color(0XFF900C3F),
+              fontSize: 16.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
